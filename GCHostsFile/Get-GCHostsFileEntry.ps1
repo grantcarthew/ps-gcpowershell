a846805cebd3d49aee58f43813e797bc3f1fefa7
Grant Carthew <grant@carthew.net>
2017-08-15 13:12:19 +1000

    Added hosts name array support

--- GCHostsFile/Get-GCHostsFileEntry.ps1
+++ GCHostsFile/Get-GCHostsFileEntry.ps1
@@ -3,21 +3,23 @@
   Returns any hosts file entries in the form of PSCustomObjects.
 .DESCRIPTION
   Reads the contents of the %SYSTEMROOT%\System32\drivers\etc\hosts file
-  returning the results as an item or array of PSCustom objects.
+  returning the results as an item or array of PS custom objects.
 
   The returned objects have the following schema:
   [PSCustomObject]@{
     IPAddress=[IPAddress];
-    HostName=[String]
+    HostName=[String[]];
   }
 
   Hosts file comments are ignored.
 
   Type 'Get-Help Get-GCHostsFileEntry -Online' for extra information.
 .PARAMETER HostName
-  Filters the result by host name.
+  Filters the result by host name. Supports a single
+  host name value or an array of host names.
 .PARAMETER IPAddress
-  Filters the result by host ip address.
+  Filters the result by host ip address. Supports a single
+  ip address or an array of ip addresses.
 .EXAMPLE
   The following example gets the list of IP addresses in the hosts file
   and stores them as strings into the $ip variable:
@@ -35,7 +37,7 @@ function Get-GCHostsFileEntry {
                ValueFromPipeline=$true,
                ValueFromPipelineByPropertyName=$true,
                ParameterSetName='ByName')]
-    [String]
+    [String[]]
     $HostName,
     
     [Parameter(Mandatory=$false,
@@ -43,49 +45,69 @@ function Get-GCHostsFileEntry {
                ValueFromPipeline=$true,
                ValueFromPipelineByPropertyName=$true,
                ParameterSetName='ByIP')]
-    [String]
+    [String[]]
     $IPAddress
   )
   
   begin {
     Import-Module -Name GCTest
     $hostsFilePath = Join-Path -Path $env:SystemRoot -ChildPath '\System32\drivers\etc\hosts'
-    $entries = New-Object -TypeName System.Collections.ArrayList
+    Write-Verbose -Message "Hosts file path set to: $hostsFilePath"
+
     if (-not (Test-GCHostsFile)) {
       Write-Error -Message "Hosts file missing or the format is invalid. Please inspect the hosts file."
     }
+
+    Write-Verbose -Message "Getting content from hosts file."
+    $lines = Get-Content -Path $hostsFilePath
+    $entries = New-Object -TypeName System.Collections.ArrayList
+    $lineElements = New-Object -TypeName System.Collections.ArrayList
   }
   
   process {
-    $rawContent = Get-Content -Path $hostsFilePath
-    foreach ($line in $rawContent) {
+    Write-Verbose -Message "Processing with IPAddress: $IPAddress"
+    Write-Verbose -Message "Processing with HostName: $HostName"
+    foreach ($line in $lines) {
       if ($line -notmatch '^$|^\s*$|^\s*#') {
-        $lineElements = $line.Trim() -split '\s+'
-        if ($lineElements.length -ge 2) {
+        $lineElements.clear()
+        $lineElements.AddRange($line.Trim() -split '\s+')
+        if ($lineElements.Count -ge 2) {
           $add = $false
-          if ($IPAddress -and $IPAddress.Trim() -eq $lineElements[0]) { $add = $true }
-          if ($HostName -and $HostName.Trim().ToUpper() -eq $lineElements[1].ToUpper()) { $add = $true } 
-          if (-not $HostName -and -not $IPAddress) { $add = $true }
+          if (-not $HostName -and -not $IPAddress) {
+            $add = $true
+          } elseif ($IPAddress -and $IPAddress.Trim() -eq $lineElements[0]) {
+            Write-Verbose -Message "IPAddress match: $($lineElements[0])"
+            $add = $true
+          } else {
+            for ($i = 1; $i -lt $lineElements.Count; $i++) {
+              if ($HostName -and $HostName.Trim().ToUpper() -eq $lineElements[$i].ToUpper()) {
+                Write-Verbose -Message "HostName match: $($lineElements[$i])"
+                $add = $true
+              } 
+            }
+          }
           if ($add) {
+            $ip = $lineElements[0]
+            $lineElements.RemoveAt(0)
             $entries.Add(
               [PSCustomObject]@{
-                IPAddress=[IPAddress]$lineElements[0];
-                HostName=[String]$lineElements[1]
+                IPAddress=[IPAddress]$ip;
+                HostName=[String[]]$lineElements.ToArray()
               }
             ) | Out-Null
           }
         }
       }
     }
-    if ($entries.Count -eq 1) {
-      Return $entries[0]
-    } elseif ($entries.Count -gt 1) {
-      Return $entries
-    } else {
-      Return $null
-    }
   }
   
   end {
+    if ($entries.Count -eq 1) {
+      Write-Output -InputObject $entries[0]
+    } elseif ($entries.Count -gt 1) {
+      Write-Output -InputObject $entries
+    } else {
+      Write-output -InputObject $null
+    }
   }
 }
