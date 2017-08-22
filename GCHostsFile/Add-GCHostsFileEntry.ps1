278fc665c9fa88845fac88dd9a8ad4ba670f557e
Grant Carthew <grant@carthew.net>
2017-08-22 15:20:11 +1000

    Working version

--- GCHostsFile/Add-GCHostsFileEntry.ps1
+++ GCHostsFile/Add-GCHostsFileEntry.ps1
@@ -8,17 +8,26 @@
   If an entry that matches both the host name and
   IP address already exists, the change will be ignored.
 
-  Note: This cmdlet does not support piping. Use 'foreach' to
-  apply multiple changes to the hosts file.
-
   Type 'Get-Help Add-GCHostsFileEntry -Online' for extra information.
+.PARAMETER InputObject
+  A PSCustomObject with the details of the hosts file entry.
+  The schema of the object will be as follows:
+  
+  [PSCustomObject]@{
+    IPAddress=[IPAddress];
+    HostNames=[String[]];
+    Comment=[String];
+  }
+
+  A single PSCustomObject or an array of PSCustomObjects can be
+  passed in for addition to the hosts file.
+.PARAMETER IPAddress
+  The IP address to assign to the host name.
+  This can be an IPv4 or IPv6 address.
 .PARAMETER HostName
   The name of the host to assign the IP address to.
   This can be a fully qualified name such as server1.domain.com, or a flat name.
   An array of names is supported.
-.PARAMETER IPAddress
-  The IP address to assign to the host name.
-  This can be an IPv4 or IPv6 address.
 .PARAMETER Comment
   Adds a comment on the same line as the IPAddress and HostName pair.
 
@@ -37,6 +46,14 @@ function Add-GCHostsFileEntry {
                 HelpUri = 'https://github.com/grantcarthew/GCPowerShell')]
   [OutputType([String])]
   Param (
+    [Parameter(Mandatory=$false,
+              Position=0,
+              ValueFromPipeline=$true,
+              ValueFromPipelineByPropertyName=$true,
+              ParameterSetName='HostObject')]
+    [PSCustomObject[]]
+    $InputObject,
+
     [Parameter(Mandatory=$true,
               Position=0,
               ValueFromPipeline=$false,
@@ -65,55 +82,99 @@ function Add-GCHostsFileEntry {
     $LineComment
   )
 
-  $hostsFilePath = Join-Path -Path $env:SystemRoot -ChildPath '\System32\drivers\etc\hosts'
-  $tab1 = 17
-  Write-Verbose -Message "Hosts file path set to: $hostsFilePath"
+  begin {
+    Write-Verbose -Message "Function initiated: $($MyInvocation.MyCommand)"
+    Import-Module -Name GCTest
 
-  if (-not (Test-GCFileWrite -Path $hostsFilePath)) {
-    Write-Error -Message "Can not write to the hosts file. Check it exists and you have write permissions."
-    Exit
-  }
+    $hostsFilePath = Join-Path -Path $env:SystemRoot -ChildPath '\System32\drivers\etc\hosts'
+    Write-Verbose -Message "Hosts file path set to: $hostsFilePath"
 
-  if ((Get-Content -Path $hostsFilePath -Raw) -notmatch "`r`n$") {
-    Write-Verbose -Message "Adding CR-LF to the end of the hosts file."
-    Add-Content -Path $hostsFilePath -Value '' -Encoding Ascii -Force
-  }
+    if (-not (Test-GCFileWrite -Path $hostsFilePath)) {
+      Write-Error -Message "Can not write to the hosts file. Check it exists and you have write permissions."
+      Exit
+    }
 
-  if ($LineComment) {
-    Write-Verbose -Message "Inserting line comment."
-    $commentString = $LineComment.TrimStart('#').Insert(0, '# ')
-    Add-Content -Path $hostsFilePath -Value $commentString
-  } else {
-    # The following will cause an error if the IPAddress is invalid.
-    Write-Verbose -Message "Type casting IPAddress."
-    [IPAddress]$IPAddress | Out-Null
+    if ((Get-Content -Path $hostsFilePath -Raw) -notmatch "`r`n$") {
+      Write-Verbose -Message "Adding CR-LF to the end of the hosts file."
+      Add-Content -Path $hostsFilePath -Value '' -Encoding Ascii -Force
+    }
 
-    Write-Verbose -Message "Testing for duplicate entry"
-    $hostsFileEntries = Get-GCHostsFileEntry -HostName $HostName
-    $addEntry = $false
+    $hostsFileEntries = Get-GCHostsFileEntry 
+    $newEntries = New-Object -TypeName System.Collections.ArrayList
+    $allHostNames = New-Object -TypeName System.Collections.ArrayList
     foreach ($entry in $hostsFileEntries) {
-        if ($entry.IPAddress.IPAddressToString -eq $IPAddress.Trim()) {
-          for ($i = 0; $i -lt $HostName.length; $i++) {
-            if ($HostName[$i] -ne $entry.HostName[$i]) {
-              Write-Verbose -Message "New entry detected: $IPAddress"
-              $addEntry = $true
-            }
-          }
-        }
-    } 
-
-    if ($addEntry) {
-      Write-Verbose -Message "Adding new entry: $IPAddress"
-      $newEntry = $IPAddress.PadRight($tab1)
-      foreach ($name in $HostName) {
-        $newEntry += " $name"
+      foreach ($n in $entry.HostNames) {
+        $allHostNames.Add($n.ToUpper().Trim()) | Out-Null
       }
-      if ($Comment) {
-        $commentString = $Comment.TrimStart('# ').Insert(0, '# ')
+    }
+    function TestNewHostsIP ($NewIP) {
+      # The following will cause an error if the IPAddress is invalid.
+      # The error will be reported to the console.
+      Write-Verbose -Message "Type casting IPAddress: $NewIP"
+      [IPAddress]$NewIP | Out-Null
+    }
+    function TestDuplicateHostName ($NewHostName) {
+      Write-Verbose -Message "Testing for duplicate entry: $NewHostName" 
+      $duplicate = $false
+      foreach ($eachName in $NewHostName) {
+        if ($eachName.ToUpper().Trim() -in $allHostNames) {
+          $duplicate = $true 
+          break
+        }  
+      }
+      Write-Output -InputObject $duplicate
+    }
+    function QueueNewHostsEntry ($NewIp, $NewNames, $NewComment) {
+      Write-Verbose -Message "Adding new entry: $NewIp,$NewNames,$NewComment"
+      $tab1 = 17
+      $newEntry = $NewIp.PadRight($tab1)
+      foreach ($NewName in $NewNames) {
+        $newEntry += " $NewName"
+      }
+      if ($NewComment) {
+        $commentString = $NewComment.TrimStart('# ').Insert(0, '# ')
         $newEntry += " $commentString" 
       }
-      Add-Content -Path $hostsFilePath -Value $newEntry -Encoding Ascii -Force
+      $newEntries.Add($newEntry) | Out-Null
     }
   }
+
+  process {
+    if ($LineComment) {
+      Write-Verbose -Message "Inserting line comment."
+      $commentString = $LineComment.TrimStart('#').Insert(0, '# ')
+      $newEntries.Add($commentString) | Out-Null
+    } elseif ($InputObject) {
+      foreach ($obj in $InputObject) {
+        TestNewHostsIP -NewIP $obj.IPAddress.IPAddressToString
+        $obj
+        if (TestDuplicateHostName -NewHostName $obj.HostNames) {
+          Write-Warning -Message "Host name already exists: $($obj.HostNames)"
+        } else {
+          QueueNewHostsEntry -NewIp $obj.IPAddress.IPAddressToString `
+                             -NewNames $obj.HostNames `
+                             -NewComment $obj.$Comment
+        }
+      }
+    } else {
+      TestNewHostsIP -NewIP $IPAddress
+
+      if (TestDuplicateHostName -NewHostName $HostName) {
+        Write-Warning -Message "Host name already exists: $HostName"
+      } else {
+        QueueNewHostsEntry -NewIp $IPAddress -NewNames $HostName -NewComment $Comment
+      }
+    }
+  }
+
+  end {
+    if ($newEntries.Count -gt 0) {
+      Add-Content -Path $hostsFilePath -Value $newEntries -Encoding Ascii -Force
+    } else {
+      Write-Warning -Message 'No host entries to add.'
+    }
+    Write-Verbose -Message "Function completed: $($MyInvocation.MyCommand)"
+  }
+
 
 }
