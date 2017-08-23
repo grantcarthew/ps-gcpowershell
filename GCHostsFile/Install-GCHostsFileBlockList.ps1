d5268e1bcd0f7747f56b3a86b696cc1474498ef6
Grant Carthew <grant@carthew.net>
2017-08-23 16:23:26 +1000

    Block list download working

--- GCHostsFile/Install-GCHostsFileBlockList.ps1
+++ GCHostsFile/Install-GCHostsFileBlockList.ps1
@@ -1,9 +1,11 @@
 <#
 .SYNOPSIS
-  Downloads a hosts file block list replacing the current hosts file. 
+  Downloads a hosts file block list replacing or appending
+  it to the current hosts file. 
 .DESCRIPTION
   This cmdlet will download a hosts file block list from github
-  and replace the current hosts file.
+  and replace or append the block list to the end of the
+  current hosts file.
 
   The source of the block list is maintained by a Steven Black:
   https://github.com/StevenBlack
@@ -15,22 +17,22 @@
 .PARAMETER Extension
   Customizes the default Adware/Malware block list adding extra
   unwanted domains.
+.PARAMETER Append
+  Adds the block list host entries to the end of the
+  existing hosts file.
 .EXAMPLE
   The following example installs the default Adware/Malware block list
-  with the addition of the fakenews extension:
+  with the addition of the FakeNews extension:
 
-  Install-GCHostFileBlockList -Extensions fakenews
+  Install-GCHostFileBlockList -Extensions FakeNews
 #>
 function Install-GCHostsFileBlockList {
-  [CmdletBinding(DefaultParameterSetName='ByExtension',
-                 HelpUri = 'https://github.com/grantcarthew/GCPowerShell')]
-  [OutputType([PSCustomObject[]])]
+  [CmdletBinding(HelpUri = 'https://github.com/grantcarthew/GCPowerShell')]
+  [OutputType([Int])]
   Param (
     [Parameter(Mandatory=$false,
                Position=0,
-               ValueFromPipeline=$true,
-               ValueFromPipelineByPropertyName=$true,
-               ParameterSetName='ByExtension')]
+               ValueFromPipeline=$false)]
     [ValidateSet('FakeNews',
                  'Gambling',
                  'Porn',
@@ -47,47 +49,67 @@ function Install-GCHostsFileBlockList {
                  'Gambling+Porn+Social',
                  'FakeNews+Gambling+Porn+Social')]
     [String]
-    $Extension
+    $Extension,
+
+    [Parameter(Mandatory=$false)]
+    [Switch]
+    $Append
   )
   
-  begin {
-    Write-Verbose -Message "Function initiated: $($MyInvocation.MyCommand)"
-    $urlRoot = 'https://raw.githubusercontent.com/StevenBlack/hosts/master'
-   switch ($Extension) {
-     'FakeNews'          { $url = $urlRoot + '/alternates/fakenews/hosts'  }
-     'Gambling'          { $url = $urlRoot + '/alternates/gambling/hosts'  }
-     'Porn'              { $url = $urlRoot + '/alternates/porn/hosts'  }
-     'Social'            { $url = $urlRoot + '/alternates/social/hosts'  }
-     'FakeNews+Gambling' { $url = $urlRoot + '/alternates/fakenews-gambling/hosts'  }
-     'FakeNews+Porn'     { $url = $urlRoot + '/alternates/fakenews-porn/hosts'  }
-     'FakeNews+Social'   { $url = $urlRoot + '/alternates/fakenews-social/hosts'  }
-     'Gambling+Porn'     { $url = $urlRoot + '/alternates/gambling-porn/hosts'  }
-     'Gambling+Social'   { $url = $urlRoot + '/alternates/gambling-social/hosts'  }
-     'Porn+Social'       { $url = $urlRoot + '/alternates/porn-social/hosts'  }
-     'FakeNews+Gambling+Porn'        { $url = $urlRoot + '/alternates/fakenews-gambling-porn/hosts'  }
-     'FakeNews+Gambling+Social'      { $url = $urlRoot + '/alternates/fakenews-gambling-social/hosts'  }
-     'FakeNews+Porn+Social'          { $url = $urlRoot + '/alternates/fakenews-porn-social/hosts'  }
-     'Gambling+Porn+Social'          { $url = $urlRoot + '/alternates/gambling-porn-social/hosts'  }
-     'FakeNews+Gambling+Porn+Social' { $url = $urlRoot + '/alternates/fakenews-gambling-porn-social/hosts'  }
-     Default { $url = $urlRoot + '/hosts' }
-   } 
+  Write-Verbose -Message "Function initiated: $($MyInvocation.MyCommand)"
+  Import-Module -Name GCTest
 
-    $hostsFilePath = Join-Path -Path $env:SystemRoot -ChildPath '\System32\drivers\etc\hosts'
-    Write-Verbose -Message "Hosts file path set to: $hostsFilePath"
+  $urlRoot = 'https://raw.githubusercontent.com/StevenBlack/hosts/master'
+  switch ($Extension) {
+    'FakeNews'          { $url = $urlRoot + '/alternates/fakenews/hosts'  }
+    'Gambling'          { $url = $urlRoot + '/alternates/gambling/hosts'  }
+    'Porn'              { $url = $urlRoot + '/alternates/porn/hosts'  }
+    'Social'            { $url = $urlRoot + '/alternates/social/hosts'  }
+    'FakeNews+Gambling' { $url = $urlRoot + '/alternates/fakenews-gambling/hosts'  }
+    'FakeNews+Porn'     { $url = $urlRoot + '/alternates/fakenews-porn/hosts'  }
+    'FakeNews+Social'   { $url = $urlRoot + '/alternates/fakenews-social/hosts'  }
+    'Gambling+Porn'     { $url = $urlRoot + '/alternates/gambling-porn/hosts'  }
+    'Gambling+Social'   { $url = $urlRoot + '/alternates/gambling-social/hosts'  }
+    'Porn+Social'       { $url = $urlRoot + '/alternates/porn-social/hosts'  }
+    'FakeNews+Gambling+Porn'        { $url = $urlRoot + '/alternates/fakenews-gambling-porn/hosts'  }
+    'FakeNews+Gambling+Social'      { $url = $urlRoot + '/alternates/fakenews-gambling-social/hosts'  }
+    'FakeNews+Porn+Social'          { $url = $urlRoot + '/alternates/fakenews-porn-social/hosts'  }
+    'Gambling+Porn+Social'          { $url = $urlRoot + '/alternates/gambling-porn-social/hosts'  }
+    'FakeNews+Gambling+Porn+Social' { $url = $urlRoot + '/alternates/fakenews-gambling-porn-social/hosts'  }
+    Default { $url = $urlRoot + '/hosts' }
+  } 
+  Write-Verbose -Message "Download URL set to: $url"
 
-    Write-Verbose -Message "Getting content from hosts file."
-    $lines = Get-Content -Path $hostsFilePath
-    $entries = New-Object -TypeName System.Collections.ArrayList
-    $lineElements = New-Object -TypeName System.Collections.ArrayList
+  $hostsFilePath = Join-Path -Path $env:SystemRoot -ChildPath '\System32\drivers\etc\hosts'
+  Write-Verbose -Message "Hosts file path set to: $hostsFilePath"
+
+  if (-not (Test-GCFileWrite -Path $hostsFilePath)) {
+    Write-Error -Message "Can't write to the hosts file. Check it exists and you have write permissions."
+    Exit
   }
-  
-  process {
-    $blockList = Invoke-WebRequest -Uri $url -UseBasicParsing
-    $blockList.content.length
+
+  $tempFileName = Get-Date -Format 'yyyy-MM-dd-hh-mm-ss-ffff'
+  $tempFilePath = Join-Path -Path $env:TEMP -ChildPath $tempFileName 
+  Write-Verbose -Message "Temp file path set to: $tempFilePath"
+
+  # The use of the temp file is to insert CR-LF.
+  Write-Verbose -Message "Downloading block list"
+  Invoke-WebRequest -Uri $url -ContentType 'text/plain' -UseBasicParsing -OutFile $tempFilePath
+  $blockList = Get-Content -Path $tempFilePath -Force
+  if ($Append) {
+    if ((Get-Content -Path $hostsFilePath -Raw) -notmatch "`r`n$") {
+      Write-Verbose -Message "Adding CR-LF to the end of the hosts file."
+      Add-Content -Path $hostsFilePath -Value '' -Encoding Ascii -Force
+    }
+    Write-Verbose -Message "Appending block list to the end of the hosts file."
+    Out-File -FilePath $hostsFilePath -InputObject $blockList -Encoding Ascii -Append -Force
+  } else {
+    Write-Verbose -Message "Replacing hosts file with the block list."
+    Out-File -FilePath $hostsFilePath -InputObject $blockList -Encoding Ascii -Force
   }
-  
-  end {
-    Write-Verbose -Message "Total hosts entries: $($entries.Count)"
-    Write-Verbose -Message "Function completed: $($MyInvocation.MyCommand)"
-  }
+
+  Remove-Item -Path $tempFilePath -Force -ErrorAction SilentlyContinue
+
+  Write-Verbose -Message "Total block list hosts entries added: $($blockList.Length)"
+  Write-Verbose -Message "Function completed: $($MyInvocation.MyCommand)"
 }
